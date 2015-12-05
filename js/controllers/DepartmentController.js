7c01ba3fdd2bf43c0dc523c67bbfe57718e3f679
tajrazak <tajrazak>
2015-12-05 20:04:11 +0530

    taj:-added code in html and contollers

--- js/controllers/DepartmentController.js
+++ js/controllers/DepartmentController.js
@@ -1,5 +1,4 @@
 angular.module('HackathonApp.controllers').controller('deptCtrl',function($scope,$http){
-	
 	$http.get('http://192.168.199.81:3000/categories_list').success(function(data){
 		console.log(data)
 		$scope.depts=data.category;
@@ -8,7 +7,10 @@ angular.module('HackathonApp.controllers').controller('deptCtrl',function($scope
 	})
 
 	$scope.submitDeptData=function(){
-		console.log($scope.myColor)
+		if($scope.myDept!=undefined||$scope.myDept!=null)
+		{
+			console.log($scope.myDept)
+		}
 	}
 
 })
