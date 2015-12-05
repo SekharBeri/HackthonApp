a91b8db1632ba797d4d0964bf6824f9320ec655e
sekharberi <sekhar.mca270@gmail.com>
2015-12-05 12:19:26 +0530

    added - module fiel

---
+++ js/module.js
@@ -0,0 +1,3 @@
+angular.module('forumApp', ['forumApp.controllers', 'ngRoute', 'ngMessages']);
+angular.module('forumApp.controllers', ['forumApp.services']);
+angular.module('forumApp.services', ['ngResource']);
