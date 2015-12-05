55e85554b5e4d7420b063f28ca0fca0b637676a6
sekharberi <sekhar.mca270@gmail.com>
2015-12-05 19:26:06 +0530

    sekhar - modification made to categories views

--- js/controllers/categories_controller.js
+++ js/controllers/categories_controller.js
@@ -1,5 +1,5 @@
 angular.module('HackathonApp.controllers')
-  .controller('CategoriesController',['$scope', '$http', function($scope, $http){
+  .controller('CategoriesController',['$scope', '$http', '$state', function($scope, $http, $state){
     
     $http({method: 'GET',
       url: "http://192.168.199.81:3000/categories_list"
@@ -9,4 +9,20 @@ angular.module('HackathonApp.controllers')
     .error(function(error){
     alert(error+"sdf");
     });
+    
+    $scope.createCategory = function(){
+      category = { name: $scope.categoryName };
+      $http({method: 'POST',
+        url: "http://192.168.199.81:3000/categories/create_category",
+        data: category
+      }).success(function(response){
+        if(response){
+          $state.go('adminAddService.categories');
+        }
+      })
+      .error(function(error){
+        alert(error+"sdf");
+      });
+    };
+    
   }]);
